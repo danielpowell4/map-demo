c71f941dd690441b341e0aaea7fafca7619f492b
Dan <danielpowell12@msn.com>
2022-10-18 14:53:46 -0600

    Adds session filter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import Head from "next/head";
 import Map from "../components/Map";
 
@@ -9,6 +11,10 @@ const DEFAULT_CENTER = [40.776924, -73.9571366];
 const fillBlueOptions = { fillColor: "blue" };
 
 export default function Home() {
+  const [minSessions, setMinSessions] = React.useState(1);
+
+  const maxSessions = Math.max(...data.map((d) => d.sessions));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,22 +26,46 @@ export default function Home() {
         <h1 className={styles.title}>UES Location</h1>
         <p>A snapshot of activity since Jan 1, 2022</p>
 
+        <ul>
+          <li>
+            <label htmlFor="starting_score">Min Sessions: {minSessions}</label>
+            <input
+              type="range"
+              id="starting_score"
+              name="starting_score"
+              min={1}
+              max={maxSessions}
+              value={minSessions}
+              onChange={(e) => setMinSessions(parseInt(e.target.value))}
+            />
+          </li>
+        </ul>
+
         <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
-          {({ TileLayer, Circle }) => (
+          {({ TileLayer, Circle, Popup }) => (
             <>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               />
               {data
+
                 .filter((d) => d && d.lat && d.long)
-                .map((d) => (
-                  <Circle
-                    center={[d.lat, d.long]}
-                    pathOptions={fillBlueOptions}
-                    radius={d.sessions}
-                  />
-                ))}
+                .filter((d) => d.sessions >= minSessions)
+                .map((d) => {
+                  const { lat, long, ...details } = d;
+                  return (
+                    <Circle
+                      center={[lat, long]}
+                      pathOptions={fillBlueOptions}
+                      radius={d.sessions}
+                    >
+                      <Popup>
+                        <pre>{JSON.stringify(details, null, 2)}</pre>
+                      </Popup>
+                    </Circle>
+                  );
+                })}
             </>
           )}
         </Map>
